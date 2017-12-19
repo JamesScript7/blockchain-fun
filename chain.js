8e39d9fc877c82dec4d9808f31295609561082e7
James Kim <james.kim7@yahoo.com>
2017-12-18 19:58:36 -0500

    create getGenesisBlock

--- chain.js
+++ chain.js
@@ -22,6 +22,13 @@ const generateNextBlock = (blockData) => {
   let previousBlock = getLatestBlock();
   let nextIndex = previousBlock.index++;
   let nextTimeStamp = new Date().getTime() / 1000;
-  var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimeStamp, blockData);
+  let nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimeStamp, blockData);
+  
   return new Block(nextIndex, previousBlock.hash, nextTimeStamp, blockData, nextHash);
 }
+
+const getGenesisBlock = () => {
+  return new Block(0, "0", 1513644863, "my genesis block", "3b733bdc915f8391a0696f86b5655f644b59680380a33ad42040e937fcd11716");
+}
+
+let blockchain = [getGenesisBlock()];
